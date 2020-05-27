4b2adfa924fc5fe756c519e901015f30c8c18ebe
gchaim <gchaimke@gmail.com>
2020-05-27 18:30:12 +0300

    menu update

--- application/views/main_menu.php
+++ application/views/main_menu.php
@@ -7,27 +7,23 @@ if (isset($this->session->userdata['logged_in'])) {
 ?>
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark main-menu">
   <a class="navbar-brand" href="/">Workflow Tag</a>
+  <ul class="navbar-nav mr-auto">
+      <?php
+      if (isset($project)) {
+        echo '<li class="nav-item project"><a class="nav-link" href="/production/checklists/' . $project . '">' . $project . '</a></li>';
+      }
+      ?>
+    </ul>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDefault" aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarDefault">
     <ul class="navbar-nav mr-auto">
-      <?php
-      if (isset($project)) {
-        echo '<li class="nav-item"><a class="nav-link" href="/production/checklists/' . $project . '">' . $project . '</a></li>';
-      }
-      ?>
+      |
     </ul>
     <ul class="navbar-nav  pull-right">
-      <?php
-      if ($role == 'Admin') {
-        echo '<li class="nav-item"><a class="nav-link" href="/production/manage_clients">Clients</a></li>';
-        echo '<li class="nav-item"><a class="nav-link" href="/production/manage_templates">Templates</a></li>';
-        echo '<li class="nav-item"><a class="nav-link" href="/users">Users</a></li>';
-        echo '<li class="nav-item"><a class="nav-link" href="/admin/settings">Settings</a></li>';
-      } ?>
-      <li class="nav-item dropdown">
+    <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Hello <?php echo $username; ?>
         </a>
@@ -36,6 +32,13 @@ if (isset($this->session->userdata['logged_in'])) {
           <a class="dropdown-item" href="/users/logout">Logout</a>
         </div>
       </li>
+      <?php
+      if ($role == 'Admin') {
+        echo '<li class="nav-item"><a class="nav-link" href="/production/manage_clients">Clients</a></li>';
+        echo '<li class="nav-item"><a class="nav-link" href="/production/manage_templates">Templates</a></li>';
+        echo '<li class="nav-item"><a class="nav-link" href="/users">Users</a></li>';
+        echo '<li class="nav-item"><a class="nav-link" href="/admin/settings">Settings</a></li>';
+      } ?>
     </ul>
   </div>
 </nav>
