9bed6ded81a600789c55262e52212e6c66bdd5a3
gchaim <gchaimke@gmail.com>
2020-06-14 10:06:09 +0300

    menu update

--- application/views/main_menu.php
+++ application/views/main_menu.php
@@ -8,12 +8,12 @@ if (isset($this->session->userdata['logged_in'])) {
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark main-menu">
   <a class="navbar-brand" href="/">Workflow Tag</a>
   <ul class="navbar-nav mr-auto">
-      <?php
-      if (isset($project)) {
-        echo '<li class="nav-item project"><a class="nav-link" href="/production/checklists/' . $project . '">' . $project . '</a></li>';
-      }
-      ?>
-    </ul>
+    <?php
+    if (isset($project)) {
+      echo '<li class="nav-item project"><a class="nav-link" href="/production/checklists/' . $project . '">' . $project . '</a></li>';
+    }
+    ?>
+  </ul>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDefault" aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -23,7 +23,7 @@ if (isset($this->session->userdata['logged_in'])) {
       |
     </ul>
     <ul class="navbar-nav  pull-right">
-    <li class="nav-item dropdown">
+      <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Hello <?php echo $username; ?>
         </a>
@@ -32,13 +32,19 @@ if (isset($this->session->userdata['logged_in'])) {
           <a class="dropdown-item" href="/users/logout">Logout</a>
         </div>
       </li>
-      <?php
-      if ($role == 'Admin') {
-        echo '<li class="nav-item"><a class="nav-link" href="/clients">Clients</a></li>';
-        echo '<li class="nav-item"><a class="nav-link" href="/templates">Templates</a></li>';
-        echo '<li class="nav-item"><a class="nav-link" href="/users">Users</a></li>';
-        echo '<li class="nav-item"><a class="nav-link" href="/admin/settings">Settings</a></li>';
-      } ?>
+      <?php if ($role == 'Admin') {?>
+        <li class="nav-item"><a class="nav-link" href="/clients">Clients</a></li>
+        <li class="nav-item"><a class="nav-link" href="/templates">Templates</a></li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Admin
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="/users">Users</a>
+        <a class="dropdown-item" href="/admin/settings">Settings</a>
+        </div>
+      </li>
+      <?php }?>
     </ul>
   </div>
 </nav>
