b5ebf41d279df4fd9cf9f2daf94b8172ccfe0233
gchaim <gchaimke@gmail.com>
2020-08-18 13:53:31 +0300

    unique serial number

--- application/models/Production_model.php
+++ application/models/Production_model.php
@@ -6,7 +6,7 @@ class Production_model extends CI_Model
 	public function addChecklist($data)
 	{
 		// Query to check whether serial already exist or not
-		$condition = "serial ='" . $data['serial'] . "' AND project='" . $data['project'] . "'";
+		$condition = "serial ='" . $data['serial'] . "'";
 		$this->db->select('*');
 		$this->db->from('Checklists');
 		$this->db->where($condition);
